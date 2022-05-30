10a1d38be2d7c705b3c23db3cb8472232fbd7390
ivivanov <ivanivanov12@gmail.com>
2022-05-30 14:01:38 +0300

    Add tests

---
+++ test/cola-machine.test.js
@@ -0,0 +1,139 @@
+const { expect, assert } = require('chai')
+const { ethers } = require('hardhat')
+const { utils, BigNumber } = require('ethers')
+const evm = require('./utils/evm.js')
+
+const FORK_BLOCK_NUMBER = 12314633
+
+describe('ColaMachine contract', function () {
+  const INITIAL_PRICE = utils.parseUnits('0.1', 'ether')
+  const INITIAL_STOCK = 5
+
+  let snapshotId
+
+  // signers
+  let deployer
+  let randomUser
+
+  // contracts
+  let colaMachine
+  let spaceCola
+
+  before(async () => {
+    // forking ropsten
+    await evm.reset({
+      jsonRpcUrl: process.env.RPC_ROPSTEN,
+      blockNumber: FORK_BLOCK_NUMBER,
+    })
+
+    // getting signers with ETH
+    ;[deployer, randomUser] = await ethers.getSigners()
+
+    // precalculating SpaceCola's contract address as both ColaMachine contract and SpaceCola contract depend on
+    // one another
+    const currentNonce = await ethers.provider.getTransactionCount(deployer.address)
+    const spaceColaPrecalculatedAddress = utils.getContractAddress({ from: deployer.address, nonce: currentNonce + 1 })
+
+    // deploy contracts
+    const colaMachineFactory = await ethers.getContractFactory('ColaMachine', deployer)
+    colaMachine = await colaMachineFactory.deploy(spaceColaPrecalculatedAddress, INITIAL_PRICE)
+
+    const spaceColaFactory = await ethers.getContractFactory('SpaceCola', deployer)
+    spaceCola = await spaceColaFactory.deploy(colaMachine.address)
+
+    // set initial stock of space cola tokens
+    await colaMachine.restock(INITIAL_STOCK)
+
+    // snapshot
+    snapshotId = await evm.snapshot.take()
+  })
+
+  beforeEach(async () => {
+    await evm.snapshot.revert(snapshotId)
+  })
+
+  /**
+   * Tests
+   * - Success cases
+   *  - Emit event tests
+   *  - Function result tests
+   * - Edge cases
+   * - Test for common vulnerabilities
+   */
+
+  // * buyBottle tests
+  // Emit event tests
+  it('should emit BottleBought when buying bottle', async () => {
+    await expect(colaMachine.connect(randomUser).buyBottle({ value: INITIAL_PRICE }))
+      .to.emit(colaMachine, 'BottleBought')
+      .withArgs(randomUser.address, 1)
+  })
+
+  // Function result tests
+  it('should get SPC token when buying bottle', async () => {
+    await colaMachine.connect(randomUser).buyBottle({ value: INITIAL_PRICE })
+    expect(await spaceCola.balanceOf(randomUser.address)).to.be.eq(1)
+  })
+
+  it('should get SPC token when buying bottle on discounted price', async () => {
+    await colaMachine.connect(randomUser).buyBottle({ value: INITIAL_PRICE })
+    await spaceCola.connect(randomUser).increaseAllowance(colaMachine.address, 1)
+    await colaMachine.connect(randomUser).returnBottle()
+
+    const discount = await colaMachine.RETURN_BOTTLE_DISCOUNT()
+    const price = calculateDiscount(discount, INITIAL_PRICE)
+    await colaMachine.connect(randomUser).buyBottle({ value: price })
+
+    expect(await spaceCola.balanceOf(randomUser.address)).to.be.eq(1)
+  })
+
+  // Ensure fail cases
+  it('should revert for not enough stock when buying bottle', async () => {
+    const discount = await colaMachine.BULK_ORDER_DISCOUNT()
+    const price = calculateDiscount(discount, INITIAL_PRICE.mul(5))
+    await colaMachine.connect(randomUser).buy5Bottles({ value: price })
+
+    await expect(colaMachine.connect(randomUser).buyBottle({ value: INITIAL_PRICE })).to.be.revertedWith('ColaM: not enough stock')
+  })
+
+  it('should revert for not price mismatch when buying bottle', async () => {
+    await expect(colaMachine.connect(randomUser).buyBottle({ value: INITIAL_PRICE.add(1) })).to.be.revertedWith(
+      'ColaM: eth does not match the price'
+    )
+  })
+
+  // * buy5Bottles tests
+  // * returnBottle tests
+  // * restock tests
+
+  // ... etc
+
+  // * prepareWithdrawal test
+  // Emit event tests
+  it('should emit ReadyToWithdraw when preparing withdrawal', async () => {
+    await expect(colaMachine.prepareWithdrawal()).to.emit(colaMachine, 'ReadyToWithdraw').withArgs(deployer.address, 0)
+  })
+
+  // Function result tests
+  it('should be able to withdraw all contract balance', async () => {
+    colaMachine.connect(randomUser).buyBottle({ value: INITIAL_PRICE })
+    assert.isTrue((await ethers.provider.getBalance(colaMachine.address)).eq(INITIAL_PRICE))
+
+    await colaMachine.prepareWithdrawal()
+    const tx = await colaMachine.withdrawPayments(deployer.address)
+    const rcpt = await tx.wait()
+    const eventAbi = ['event Withdrawn(address indexed payee, uint256 weiAmount)']
+    const iface = new hre.ethers.utils.Interface(eventAbi)
+
+    expect(iface.parseLog(rcpt.events[0]).args.payee).to.be.eq(deployer.address)
+    expect(iface.parseLog(rcpt.events[0]).args.weiAmount).to.be.eq(INITIAL_PRICE)
+  })
+
+  // Ensure fail cases
+  // ...
+})
+
+const calculateDiscount = (discount, price) => {
+  const percentBaseBn = BigNumber.from(100)
+  return BigNumber.from(price).div(percentBaseBn).mul(BigNumber.from(discount))
+}
--- test/operated.test.js
+++ test/operated.test.js
@@ -1,19 +1,81 @@
-// const { expect } = require('chai')
-// const { ethers } = require('hardhat')
+const { expect, assert } = require('chai')
+const { ethers } = require('hardhat')
+const { utils } = require('ethers')
+const evm = require('./utils/evm.js')
 
-// describe('Operated contract', function () {
-//   it("Should return the new greeting once it's changed", async function () {
-//     const Greeter = await ethers.getContractFactory('Greeter')
-//     const greeter = await Greeter.deploy('Hello, world!')
-//     await greeter.deployed()
+const FORK_BLOCK_NUMBER = 12314633
 
-//     expect(await greeter.greet()).to.equal('Hello, world!')
+describe('Operated abstract contract', function () {
+  const INITIAL_PRICE = utils.parseUnits('0.1', 'ether')
+  const MAX_OPERATORS = 3
 
-//     const setGreetingTx = await greeter.setGreeting('Hola, mundo!')
+  let snapshotId
 
-//     // wait until the transaction is mined
-//     await setGreetingTx.wait()
+  // signers
+  let deployer
+  let rn1
 
-//     expect(await greeter.greet()).to.equal('Hola, mundo!')
-//   })
-// })
+  // contracts
+  let colaMachine
+
+  before(async () => {
+    // forking ropsten
+    await evm.reset({
+      jsonRpcUrl: process.env.RPC_ROPSTEN,
+      blockNumber: FORK_BLOCK_NUMBER,
+    })
+
+    // getting signers with ETH
+    ;[deployer, rn1, rn2, rn3] = await ethers.getSigners()
+
+    // deploy contracts
+    const colaMachineFactory = await ethers.getContractFactory('ColaMachine', deployer)
+    colaMachine = await colaMachineFactory.deploy(ethers.constants.AddressZero, INITIAL_PRICE)
+
+    // snapshot
+    snapshotId = await evm.snapshot.take()
+  })
+
+  beforeEach(async () => {
+    await evm.snapshot.revert(snapshotId)
+  })
+
+  /**
+   * Tests
+   * - Success cases
+   *  - Emit event tests
+   *  - Function result tests
+   * - Ensure fail cases
+   * - Test for common vulnerabilities
+   */
+
+  // * addOperator tests
+  // Emit event tests
+  it('should emit OperatorAdded when adding new operator', async () => {
+    await expect(colaMachine.addOperator(rn1.address)).to.emit(colaMachine, 'OperatorAdded').withArgs(rn1.address)
+  })
+
+  // Function result tests
+  it('should increase operators counter when adding new operator', async () => {
+    assert.equal(await colaMachine.getOperatorsCount(), 1)
+
+    await colaMachine.addOperator(rn1.address)
+
+    expect(await colaMachine.getOperatorsCount()).to.be.eq(2)
+  })
+
+  // Ensure fail cases
+  it('should revert when adding new operator with senders address', async () => {
+    await expect(colaMachine.addOperator(deployer.address)).to.be.revertedWith('Operated: new address can not be sender')
+  })
+
+  it('should revert when adding new operator and max number of operators is reached', async () => {
+    await colaMachine.addOperator(rn1.address)
+    await colaMachine.addOperator(rn2.address)
+    assert.equal(await colaMachine.getOperatorsCount(), MAX_OPERATORS)
+
+    await expect(colaMachine.addOperator(rn3.address)).to.be.revertedWith('Operated: max operators reached')
+  })
+
+  // * removeOperator tests
+})
