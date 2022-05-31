e9b2298e88c18d8962d6ed68b5fe83e9411783c1
ivivanov <ivanivanov12@gmail.com>
2022-05-31 14:20:03 +0300

    Refactor buy functions

--- contracts/ColaMachine.sol
+++ contracts/ColaMachine.sol
@@ -16,9 +16,6 @@ import './TotalCounters.sol';
 contract ColaMachine is Operated, TotalCounters, PullPayment, ReentrancyGuard, IColaMachine {
   using SafeMath for uint256;
 
-  address public immutable spaceCola;
-  address public immutable daiToken;
-
   uint8 public constant MAX_CAPACITY = 20; // Max amount of bottles tokens held in the contract
   uint8 public constant PERCENT_BASE = 100; // Divisor when calculating the discount %
   uint8 public constant RETURN_BOTTLE_DISCOUNT = 50; // 50%
@@ -26,10 +23,13 @@ contract ColaMachine is Operated, TotalCounters, PullPayment, ReentrancyGuard, I
   uint8 public constant ONE_BOTTLE = 1; // Token equivalent of 1 bottle
   uint8 public constant FIVE_BOTTLES = ONE_BOTTLE * 5; // Token equivalent of 5 bottles
 
-  mapping(address => uint256) public addressToBottlesReturned;
+  address public immutable spaceCola;
+  address public immutable daiToken;
 
   uint256 public price; // Bottle price in ETH
-  uint256 public priceDAI = 18 ether; // Bottle price in DAI. Random number just for POC
+  uint256 public priceDAI; // Bottle price in DAI
+
+  mapping(address => uint256) public addressToBottlesReturned;
 
   /**
    * @dev Sets the values for space cola address and initial price of the {SpaceCola} token.
@@ -46,6 +46,7 @@ contract ColaMachine is Operated, TotalCounters, PullPayment, ReentrancyGuard, I
     spaceCola = _spaceCola;
     daiToken = _daiToken;
     price = initialPrice;
+    priceDAI = 18 ether; // Random number just for POC
   }
 
   /**
@@ -59,59 +60,40 @@ contract ColaMachine is Operated, TotalCounters, PullPayment, ReentrancyGuard, I
   /**
    * @dev See {IColaMachine-buyBottle}.
    * Will do {RETURN_BOTTLE_DISCOUNT} when the buyer has returned bottles.
-   * Throws if sent ETH does not match exactly the price with or without discount.
+   * Throws if sent ETH does not match exactly the price with/without discount.
    */
-  function buyBottle() external payable override nonReentrant minStock(1) {
-    (uint256 actualPrice, bool isDiscounted) = _calc1BottlePrice(price);
-    require(msg.value == actualPrice, 'ColaM: eth does not match the price');
+  function buyBottle() external payable override nonReentrant minStock(ONE_BOTTLE) {
+    require(msg.value == _calc1BottlePrice(price), 'ColaM: eth does not match the price');
 
-    if (isDiscounted) {
-      addressToBottlesReturned[msg.sender]--;
-    }
-
-    bool success = SpaceCola(spaceCola).transfer(msg.sender, ONE_BOTTLE);
-    if (!success) revert FailedTransfer();
-    _incrementCounters(ONE_BOTTLE);
-
-    emit BottleBought(msg.sender, ONE_BOTTLE);
+    _finalizeBuy(ONE_BOTTLE);
   }
 
   /**
    * @dev See {IColaMachine-buyBottleDAI}.
    * Will do {RETURN_BOTTLE_DISCOUNT} when the buyer has returned bottles.
-   * Throws if sent ETH does not match exactly the price with or without discount.
+   * Throws if sent ETH does not match exactly the price with/without discount.
    */
-  function buyBottleDAI(uint256 amount) external override nonReentrant minStock(1) {
-    (uint256 actualPrice, bool isDiscounted) = _calc1BottlePrice(priceDAI);
+  function buyBottleDAI(uint256 amount) external override nonReentrant minStock(ONE_BOTTLE) {
+    uint256 actualPrice = _calc1BottlePrice(priceDAI);
     require(amount == actualPrice, 'ColaM: DAI amount does not match the price');
 
-    if (isDiscounted) {
-      addressToBottlesReturned[msg.sender]--;
-    }
-
-    bool success = false;
-    success = IERC20(daiToken).transferFrom(msg.sender, _myAddress(), actualPrice);
+    bool success = IERC20(daiToken).transferFrom(msg.sender, _myAddress(), actualPrice);
     if (!success) revert FailedTransfer();
-    success = SpaceCola(spaceCola).transfer(msg.sender, ONE_BOTTLE);
-    if (!success) revert FailedTransfer();
-    _incrementCounters(ONE_BOTTLE);
 
-    emit BottleBought(msg.sender, ONE_BOTTLE);
+    _finalizeBuy(ONE_BOTTLE);
   }
 
   /**
    * @dev See {IColaMachine-buy5Bottles}.
    *
-   * Will do {BULK_ORDER_DISCOUNT} discount. Throws if sent ETH does not match exactly the price.
+   * Will apply always {BULK_ORDER_DISCOUNT} discount.
+   *
+   * Throws if sent ETH does not match exactly the price.
    */
-  function buy5Bottles() external payable override nonReentrant minStock(5) {
+  function buy5Bottles() external payable override nonReentrant minStock(FIVE_BOTTLES) {
     require(msg.value == _calc5BottlesPrice(), 'ColaM: eth does not match the price');
 
-    bool success = SpaceCola(spaceCola).transfer(msg.sender, FIVE_BOTTLES);
-    if (!success) revert FailedTransfer();
-    _incrementCounters(FIVE_BOTTLES);
-
-    emit BottleBought(msg.sender, FIVE_BOTTLES);
+    _finalizeBuy(FIVE_BOTTLES);
   }
 
   /**
@@ -187,15 +169,44 @@ contract ColaMachine is Operated, TotalCounters, PullPayment, ReentrancyGuard, I
     if (!success) revert FailedTransfer();
   }
 
-  function _calc5BottlesPrice() private view returns (uint256) {
-    return price.mul(FIVE_BOTTLES).mul(BULK_ORDER_DISCOUNT).div(PERCENT_BASE);
+  /**
+   * @dev Sends Cola tokens to the payee.
+   *
+   * Emits BottleBought event if successful.
+   */
+  function _finalizeBuy(uint8 amount) private {
+    require(amount > 0, 'ColaM: buying zero amount');
+
+    if (amount == ONE_BOTTLE && _hasReturnedBottles()) {
+      addressToBottlesReturned[msg.sender]--;
+    }
+
+    bool success = SpaceCola(spaceCola).transfer(msg.sender, amount);
+    if (!success) revert FailedTransfer();
+
+    _incrementCounters(amount);
+
+    emit BottleBought(msg.sender, amount);
   }
 
-  function _calc1BottlePrice(uint256 basePrice) private view returns (uint256 newPrice, bool isDiscounted) {
-    isDiscounted = addressToBottlesReturned[msg.sender] > 0;
-    newPrice = isDiscounted ? basePrice.mul(RETURN_BOTTLE_DISCOUNT).div(PERCENT_BASE) : basePrice;
+  function _calc5BottlesPrice() private view returns (uint256) {
+    return _calcPrice(price, FIVE_BOTTLES, BULK_ORDER_DISCOUNT);
+  }
 
-    return (newPrice, isDiscounted);
+  function _calc1BottlePrice(uint256 basePrice) private view returns (uint256) {
+    return _hasReturnedBottles() ? _calcPrice(basePrice, ONE_BOTTLE, RETURN_BOTTLE_DISCOUNT) : basePrice;
+  }
+
+  function _calcPrice(
+    uint256 _price,
+    uint8 amount,
+    uint8 discount
+  ) private pure returns (uint256) {
+    return discount == 0 ? _price.mul(amount) : _price.mul(amount).mul(discount).div(PERCENT_BASE);
+  }
+
+  function _hasReturnedBottles() private view returns (bool) {
+    return addressToBottlesReturned[msg.sender] > 0;
   }
 
   function _currentStock() private view returns (uint256) {
