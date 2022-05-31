857ec5e1186772ffed402ffa3a1381a663fe2b25
ivivanov <ivanivanov12@gmail.com>
2022-05-31 15:11:05 +0300

    Fix deployment scripts

--- deploy/01_deploy_cola_machine.js
+++ deploy/01_deploy_cola_machine.js
@@ -3,6 +3,7 @@ const { ethers } = require('hardhat')
 const { shouldVerifyContract } = require('../utils/deploy')
 
 module.exports = async (hre) => {
+  const dai = process.env.DAI_TOKEN_ROPSTEN ?? '0x0000000000000000000000000000000000000001'
   const { deployer } = await hre.getNamedAccounts()
   const INITIAL_PRICE = utils.parseUnits('0.1', 'ether')
   const currentNonce = await ethers.provider.getTransactionCount(deployer)
@@ -11,14 +12,14 @@ module.exports = async (hre) => {
   const contract = await hre.deployments.deploy('ColaMachine', {
     from: deployer,
     log: true,
-    args: [spaceColaPrecalculatedAddress, INITIAL_PRICE],
+    args: [spaceColaPrecalculatedAddress, dai, INITIAL_PRICE],
     skipIfAlreadyDeployed: true,
   })
 
   if (hre.network.name !== 'hardhat' && (await shouldVerifyContract(contract))) {
     await hre.run('verify:verify', {
       address: contract.address,
-      constructorArguments: [spaceColaPrecalculatedAddress, INITIAL_PRICE],
+      constructorArguments: [spaceColaPrecalculatedAddress, dai, INITIAL_PRICE],
     })
   }
 }
